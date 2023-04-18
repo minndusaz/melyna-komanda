0541134704eb5dacaf471daeb32be0917ab01bc6
KarolisTam <karolistamu@gmail.com>
2023-04-18 12:02:03 +0300

    karolis

---
+++ saldytuvas.py
@@ -0,0 +1,39 @@
+import os
+
+saldytuvas = {"mesa":1}
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def prideti_produkta(pavadinimas, kiekis):
+    global saldytuvas
+    if pavadinimas in saldytuvas:
+        saldytuvas[pavadinimas] += kiekis
+    else:
+        saldytuvas[pavadinimas] = kiekis
+
+def ivesti_produkta():
+    pavadinimas = input("Įveskite produktą: ")
+    kiekis = float(input("Įveskite produkto kiekį: "))
+clear()
+while True:
+    print("labas pasirinkite ka norite padaryti?")
+    print("1 - perziureti saldytuva", "2 - prideti produka", "3 - issimti produkta", "4 - suskaiciuoti turinio svori", "9 - iseiti" )
+    user_input = int(input("Irasykite savo pasirinkima: "))
+    if user_input == 1:
+        clear()
+        print(saldytuvas)
+    if user_input == 2:
+        pavadinimas = input("Įveskite produktą: ")
+        kiekis = float(input("Įveskite produkto kiekį: "))
+        prideti_produkta(pavadinimas=[], kiekis=[])
+    if user_input == 3:
+        clear()
+        print(user_input)
+    if user_input == 4:
+        clear()
+        print(user_input)
+    # if user_input != 1 or 2 or 3 or 4:
+    #     break

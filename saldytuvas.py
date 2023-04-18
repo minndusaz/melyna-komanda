9ebe5f2aa524580fbaf0f879ef7ebb61ffe43342
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-04-18 14:47:25 +0300

    update

--- saldytuvas.py
+++ saldytuvas.py
@@ -21,7 +21,6 @@ def issimti_produkta(saldytuvas, pavadinimas, kiekis):
         saldytuvas[pavadinimas] = kiekis
     return saldytuvas
 
-
 def ivesti_produkta():
     pavadinimas = input("Įveskite produktą: ")
     kiekis = float(input("Įveskite produkto kiekį: "))
@@ -31,10 +30,13 @@ def prideti(saldytuvas, pavadinimas, kiekis):
     saldytuvas[pavadinimas] = kiekis
     return saldytuvas
 
-# def tuscia(saldytuvas, kiekis):
-#     poros = list(saldytuvas.items())
-#     for raktas in poros:
-#         if 
+def tuscias(pavadinimas, kiekis):
+    poros = list(pavadinimas.items())
+    for raktas, reiksme_zodyje in poros:
+        if reiksme_zodyje <= kiekis:
+            del pavadinimas[raktas]
+    return pavadinimas
+saldytuvas = tuscias(saldytuvas, 0)
 
 clear()
 while True:
@@ -42,14 +44,25 @@ while True:
     print("1 - peržiūrėti šaldytuvą", "2 - pridėti produktą", "3 - išimti produktą", "4 - suskaičiuoti turinio svorį", "9 - išeiti" )
     user_input = int(input("Įrašykite savo pasirinkimą: "))
     if user_input == 1:
+        clear()
         print(saldytuvas)
     if user_input == 2:
+        clear()
         pavadinimas, kiekis = ivesti_produkta()
         saldytuvas = prideti_produkta(saldytuvas, pavadinimas, kiekis)
         print(saldytuvas)
     if user_input == 3:
+        clear()
+        print(saldytuvas)
         pavadinimas, kiekis = ivesti_produkta()
         saldytuvas = issimti_produkta(saldytuvas, pavadinimas, kiekis)
+    if user_input == 4:
+        turis = sum(saldytuvas.values())
+        print(f"Viso šaldytuve esančių produktų svoris: ",turis)
+    if user_input == 5:
+        clear()
+        tuscias(saldytuvas, 0)
+        print(saldytuvas)
     if user_input == 9:
         clear()
         break

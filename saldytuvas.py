55144e6e784808249bd1609264c1b7ed85db982d
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-04-18 13:50:22 +0300

    update

--- saldytuvas.py
+++ saldytuvas.py
@@ -25,9 +25,9 @@ def prideti(saldytuvas, pavadinimas, kiekis):
 
 clear()
 while True:
-    print("labas pasirinkite ka norite padaryti?")
-    print("1 - perziureti saldytuva", "2 - prideti produka", "3 - issimti produkta", "4 - suskaiciuoti turinio svori", "9 - iseiti" )
-    user_input = int(input("Irasykite savo pasirinkima: "))
+    print("Sveiki, pasirinkite ką norite padaryti?")
+    print("1 - peržiūrėti šaldytuvą", "2 - pridėti produktą", "3 - išimti produktą", "4 - suskaičiuoti turinio svorį", "9 - išeiti" )
+    user_input = int(input("Įrašykite savo pasirinkimą: "))
     if user_input == 1:
         print(saldytuvas)
     if user_input == 2:

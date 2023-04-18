b4dfe4b0a7fb2ef0137a010abbb98d0e79a9fc8b
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-04-18 13:41:20 +0300

    update

--- saldytuvas.py
+++ saldytuvas.py
@@ -7,33 +7,34 @@ def clear():
     else:
         os.system('clear')
 
-def prideti_produkta(pavadinimas, kiekis):
-    global saldytuvas
+def prideti_produkta(saldytuvas, pavadinimas, kiekis):
     if pavadinimas in saldytuvas:
         saldytuvas[pavadinimas] += kiekis
     else:
         saldytuvas[pavadinimas] = kiekis
+    return saldytuvas
 
 def ivesti_produkta():
     pavadinimas = input("Įveskite produktą: ")
     kiekis = float(input("Įveskite produkto kiekį: "))
+    return pavadinimas, kiekis
+
+def prideti(saldytuvas, pavadinimas, kiekis):
+    saldytuvas[pavadinimas] = kiekis
+    return saldytuvas
 
 clear()
 while True:
-    print("Sveiki, pasirinkite ką norite padaryti?")
-    print("1 - peržiūrėti šaldytuvą", "2 - pridėti produktą", "3 - išimti produktą", "4 - suskaičiuoti turinio svorį", "9 - išeiti" )
+    print("labas pasirinkite ka norite padaryti?")
+    print("1 - perziureti saldytuva", "2 - prideti produka", "3 - issimti produkta", "4 - suskaiciuoti turinio svori", "9 - iseiti" )
     user_input = int(input("Irasykite savo pasirinkima: "))
     if user_input == 1:
-        # clear()
         print(saldytuvas)
     if user_input == 2:
-        print(prideti_produkta(pavadinimas="", kiekis=""))
+        pavadinimas, kiekis = ivesti_produkta()
+        saldytuvas = prideti_produkta(saldytuvas, pavadinimas, kiekis)
     if user_input == 3:
-        # os.system('cls')
-        print(user_input)
-    if user_input == 4:
         clear()
-        print(user_input)
     if user_input == 9:
         clear()
         break

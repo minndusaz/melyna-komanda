f74481e4b97fe7d3133c56ec32317600a58ae891
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-04-18 11:51:26 +0300

    update

--- saldytuvas.py
+++ saldytuvas.py
@@ -24,15 +24,18 @@ while True:
     print("1 - perziureti saldytuva", "2 - prideti produka", "3 - issimti produkta", "4 - suskaiciuoti turinio svori", "9 - iseiti" )
     user_input = int(input("Irasykite savo pasirinkima: "))
     if user_input == 1:
-        clear()
+        # clear()
         print(saldytuvas)
     if user_input == 2:
         print(prideti_produkta(pavadinimas="", kiekis=""))
     if user_input == 3:
-        os.system('cls')
+        # os.system('cls')
         print(user_input)
     if user_input == 4:
-        os.system('cls')
+        clear()
         print(user_input)
+    if user_input == 9:
+        clear()
+        break
     # if user_input != 1 or 2 or 3 or 4:
     #     break

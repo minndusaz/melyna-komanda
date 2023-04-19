8609d6bb488ce5cbf6e1f4d2a473608b5e87541e
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-04-19 08:29:47 +0300

    update

--- saldytuvas.py
+++ saldytuvas.py
@@ -1,6 +1,6 @@
 import os
 
-saldytuvas = {"mesa":1}
+saldytuvas = {"mesa": 1.0, "zuvis": 0.0, "sviestas": 0.0, "kebabai": 2.0}
 def clear():
     if os.name == 'nt':
         os.system('cls')
@@ -26,9 +26,13 @@ def ivesti_produkta():
     kiekis = float(input("Įveskite produkto kiekį: "))
     return pavadinimas, kiekis
 
-def prideti(saldytuvas, pavadinimas, kiekis):
-    saldytuvas[pavadinimas] = kiekis
-    return saldytuvas
+def tuscias(pavadinimas, kiekis):
+    poros = list(pavadinimas.items())
+    for raktas, reiksme_zodyje in poros:
+        if reiksme_zodyje <= kiekis:
+            del pavadinimas[raktas]
+    return pavadinimas
+saldytuvas = tuscias(saldytuvas, 0)
 
 def gauti_produktus(saldytuvas):
     produktu_eilute = ""
@@ -36,14 +40,22 @@ def gauti_produktus(saldytuvas):
         produktu_eilute += produktas + ":" + str(kiekis) + " "
     return produktu_eilute
 
+def ar_uztenka(saldytuvas, pavadinimas, kiekis):
+    ivesti_produkta()
+
 clear()
 while True:
     print("\nPasirinkite veiksmą: \n")
-    print("1 - peržiūrėti šaldytuvą", "\n2 - pridėti produktą", "\n3 - išimti produktą", "\n4 - suskaičiuoti turinio svorį", "\n9 - išeiti" )
+    print("1 - peržiūrėti šaldytuvą", 
+        "\n2 - pridėti produktą", 
+        "\n3 - išimti produktą", 
+        "\n4 - suskaičiuoti turinio svorį", 
+        "\n5 - Patikrinti ar užtenka ingredientų receptui"
+        "\n9 - išeiti" )
     user_input = int(input("Įrašykite savo pasirinkimą: "))
     if user_input == 1:
         clear()
-        print("Šaldytuve šiuo metu yra: ", gauti_produktus(saldytuvas))
+        print(saldytuvas)
     if user_input == 2:
         clear()
         print(saldytuvas)
@@ -57,7 +69,7 @@ while True:
         print(saldytuvas)
         pavadinimas, kiekis = ivesti_produkta()
         saldytuvas = pasalinti_produkta(saldytuvas, pavadinimas, kiekis)
-        pasalinti_produkta(saldytuvas, 0)
+        tuscias(saldytuvas, 0)
         print("Šaldytuve šiuo metu yra: ", saldytuvas)
     if user_input == 4:
         clear()
